a11e56f8a294e08797647f6bc7e94de54dfa86eb
Bureiko <167917949+goryblake@users.noreply.github.com>
2024-08-29 02:17:13 -0300

    Add files via upload

---
+++ debug.py
@@ -0,0 +1,8 @@
+ 
+def leitura_arquivo ():
+    import ipdb;ipdb.set_trace()
+    with open ("wordlist.txt", "r") as arquivo:
+        for i in range (0,10):
+            print(f"senha : {arquivo.readline()}")
+ 
+leitura_arquivo()
