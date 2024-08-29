8b968554f62ebb2fc26700aa3601b5268e96f28c
Bureiko <167917949+goryblake@users.noreply.github.com>
2024-08-29 03:32:53 -0300

    Add files via upload

---
+++ portscan.py
@@ -0,0 +1,12 @@
+import socket
+
+ip = "192.10.30.8"
+
+for porta in range(1,65536):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = s.connect_ex ((ip, porta))
+    if result == 0:
+        print("{} open/aberta".format(porta))
+    else:
+        pass
+
